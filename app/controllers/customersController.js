53a05b56e5acb4c2fd42b863dd266fd75accd0cd
Jodi Detch <jodidetch@gmail.com>
2014-08-07 14:21:27 -0400

    App has controller; injected customersFactory into it

---
+++ app/controllers/customersController.js
@@ -0,0 +1,33 @@
+(function customersControllerIIFE(){
+
+  // 1. Inject the customersFactory into this controller
+  var CustomersController = function($scope, customersFactory){ //uses customersFactory
+    $scope.sortBy = "name";
+    $scope.reverse = false;
+    // 2. Create an empty customers Array in the scope.
+    $scope.customers= [];
+
+    // 3. Create a function that will set the customers Array in the scope
+    // from the customersFactory
+    function init(){
+      // Init the customers from the factory
+      $scope.customers = customersFactory.getCustomers();
+    }
+
+    // 4. Initialize the controller.
+    init();
+
+    $scope.doSort = function(propName){
+      $scope.sortBy = propName;
+      $scope.reverse = !$scope.reverse;
+    };
+
+  };
+
+ // Prevent the minifier from breaking dependency injection.
+ CustomersController.$inject = ['$scope', 'customersFactory'];
+
+ // The Controller is part of the module.
+ angular.module('customersApp').controller('customersController', CustomersController);
+
+})();
